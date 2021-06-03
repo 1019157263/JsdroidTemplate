2882b43d8589e1c021cc88658d03c05675b33c97
coolwho <44185539@qq.com>
2021-06-04 04:04:49 +0800

    Update MainActivity.java

--- app/src/main/java/com/jsdroid/template/MainActivity.java
+++ app/src/main/java/com/jsdroid/template/MainActivity.java
@@ -2,10 +2,6 @@ package com.jsdroid.template;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.ComponentActivity;
-
 import com.jsdroid.core.JsdApp;
 import com.jsdroid.ui.UiActivity;
 
