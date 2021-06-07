a7fde6f85fe6a03964e8ffd408f342291f7872a9
coolwho <44185539@qq.com>
2021-06-08 02:11:34 +0800

    Update MainActivity.java

--- app/src/main/java/com/jsdroid/template/MainActivity.java
+++ app/src/main/java/com/jsdroid/template/MainActivity.java
@@ -1,9 +1,17 @@
 package com.jsdroid.template;
 
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
 
 import com.jsdroid.core.JsdApp;
 import com.jsdroid.ui.UiActivity;
+
+import java.net.URL;
+import java.nio.ByteBuffer;
+
+import dalvik.system.InMemoryDexClassLoader;
 
 public class MainActivity extends UiActivity {
 
@@ -12,5 +20,16 @@ public class MainActivity extends UiActivity {
         super.onCreate(savedInstanceState);
         JsdApp.getInstance().runLastScript();
         JsdApp.getInstance().loadAssetsScript();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            new InMemoryDexClassLoader(ByteBuffer.allocate(1),null);
+        }
+    }
+    class U extends ClassLoader{
+        @Override
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+            defineClass();
+            return super.findClass(name);
+        }
+
     }
 }
